a9949e1ab5609a0ee6917887ce09c629a4dcbcdd
pprutzer7 <pprutzer2014@fau.edu>
2016-09-03 16:53:29 -0400

    Add files via upload

--- musicgloves.ino
+++ musicgloves.ino
@@ -1,4 +1,4 @@
-int notes [3];
+int notes [4];
 int note;
 int flex1;
 int flex2;
@@ -10,7 +10,7 @@ int flexbaseline;
 void sawToothNote(int note);
 void sawToothDoubleNote(int note1, int note2);
 void sawToothTripleNote(int note1, int note2, int note3);
-//void sawToothQuadNote(int note1, int note2, int note3, int note4);
+void sawToothQuadNote(int note1, int note2, int note3, int note4);
 
 void setup() {
   pinMode(9, OUTPUT);
@@ -19,10 +19,10 @@ void setup() {
   notes[0] = 60;
   notes[1] = 50;
   notes[2] = 40;
-  //notes[3] = 150;
+  notes[3] = 30;
   x = 0;
   y = 0;
-  flexbaseline = (analogRead(0) + analogRead(1) + analogRead(2) /*+ analogRead(3)*/)/3;
+  flexbaseline = (analogRead(0) + analogRead(1) + analogRead(2) + analogRead(3))/4;
   //analogReference(INTERNAL2V56);
 }
 
@@ -31,8 +31,8 @@ void loop() {
     flex1 = analogRead(0);
     flex2 = analogRead(1);
     flex3 = analogRead(2);
-    //flex4 = analogRead(3);
-    flexbaseline = (flexbaseline + (flex1 + flex2 + flex3 /*+ flex4*/)/3)/2;
+    flex4 = analogRead(3);
+    flexbaseline = (flexbaseline + (flex1 + flex2 + flex3 + flex4)/4)/2;
     }
     //Serial.println("flexbaseline");
     //Serial.println(flexbaseline);
@@ -44,92 +44,82 @@ void loop() {
   flex1 = 0;
   flex2 = 0;
   flex3 = 0;
-  //flex4 = 0;
+  flex4 = 0;
 
   for(int i = 0; i < 10; i++){
     flex1 += analogRead(0);
     flex2 += analogRead(1);
     flex3 += analogRead(2);
-    //flex4 += analogRead(3);
+    flex4 += analogRead(3);
   }
   flex1 = flex1/10;
   flex2 = flex2/10;
   flex3 = flex3/10;
-  //flex4 = flex4/10;
+  flex4 = flex4/10;
 
   
-  while(flex1 > (flexbaseline + 50) || flex2 > (flexbaseline + 50) || (flex3 > (flexbaseline + 50))/* || flex4 < flexbaseline*/){
-    if(flex1 > (flexbaseline + 50) && flex2 < (flexbaseline + 50) && flex3 < (flexbaseline + 50) /*&& flex4 > flexbaseline*/){
+  while(flex1 > (flexbaseline + 50) || flex2 > (flexbaseline + 50) || (flex3 > (flexbaseline + 50)) || flex4 > (flexbaseline + 50)){
+    if(flex1 > (flexbaseline + 50) && flex2 < (flexbaseline + 50) && flex3 < (flexbaseline + 50) && flex4 < (flexbaseline + 50)){
       sawToothNote(0);
     }
-    if(flex2 > (flexbaseline + 50) && flex1 < (flexbaseline + 50) && flex3 < (flexbaseline + 50)/* && flex4 > flexbaseline*/){
+    if(flex2 > (flexbaseline + 50) && flex1 < (flexbaseline + 50) && flex3 < (flexbaseline + 50) && flex4 < (flexbaseline + 50)){
       sawToothNote(1);
     }
-    if(flex3 > (flexbaseline + 50) && flex1 < (flexbaseline + 50) && flex2 < (flexbaseline + 50)/* && flex4 > flexbaseline*/){
+    if(flex3 > (flexbaseline + 50) && flex1 < (flexbaseline + 50) && flex2 < (flexbaseline + 50) && flex4 < (flexbaseline + 50)){
       sawToothNote(2);
-    }/*
-    if(flex4 < flexbaseline && flex1 > flexbaseline && flex2 > flexbaseline && flex3 > flexbaseline){
+    }
+    if(flex4 > (flexbaseline + 50) && flex1 < (flexbaseline + 50) && flex2 < (flexbaseline + 50) && flex3 < (flexbaseline + 50)){
       sawToothNote(3);
-    }*/
-    if(flex1 > (flexbaseline + 50) && flex2 > (flexbaseline + 50) && flex3 < (flexbaseline + 50) /*&& flex4 > flexbaseline*/){
+    }
+    if(flex1 > (flexbaseline + 50) && flex2 > (flexbaseline + 50) && flex3 < (flexbaseline + 50) && flex4 < (flexbaseline + 50)){
       sawToothDoubleNote(0,1);
     }
-    if(flex1 > (flexbaseline + 50) && flex2 < (flexbaseline + 50) && flex3 > (flexbaseline + 50)/* && flex4 > flexbaseline*/){
+    if(flex1 > (flexbaseline + 50) && flex2 < (flexbaseline + 50) && flex3 > (flexbaseline + 50) && flex4 < (flexbaseline + 50)){
       sawToothDoubleNote(0,2);
     }
-    /*if(flex1 < flexbaseline && flex2 > flexbaseline && flex3 > flexbaseline && flex4 < flexbaseline){
+    if(flex1 > (flexbaseline + 50) && flex2 < (flexbaseline + 50) && flex3 < (flexbaseline + 50) && flex4 > (flexbaseline + 50)){
       sawToothDoubleNote(0,3);
-    }*/
-    if(flex1 < (flexbaseline + 50) && flex2 > (flexbaseline + 50) && flex3 > (flexbaseline + 50)/* && flex4 > flexbaseline*/){
+    }
+    if(flex1 < (flexbaseline + 50) && flex2 > (flexbaseline + 50) && flex3 > (flexbaseline + 50) && flex4 < (flexbaseline + 50)){
       sawToothDoubleNote(1,2);
-    }/*
-    if(flex1 > (flexbaseline + 50) && flex2 < (flexbaseline + 50) && flex3 > (flexbaseline + 50) && flex4 < flexbaseline){
+    }
+    if(flex1 < (flexbaseline + 50) && flex2 > (flexbaseline + 50) && flex3 < (flexbaseline + 50) && flex4 > (flexbaseline + 50)){
       sawToothDoubleNote(1,3);
     }
-    if(flex1 > flexbaseline && flex2 > flexbaseline && flex3 < flexbaseline && flex4 < flexbaseline){
+    if(flex1 < (flexbaseline + 50) && flex2 < (flexbaseline + 50) && flex3 > (flexbaseline + 50) && flex4 > (flexbaseline + 50)){
       sawToothDoubleNote(2,3);
-    }*/
-    if(flex1 > (flexbaseline + 50) && flex2 > (flexbaseline + 50) && flex3 > (flexbaseline + 50)/* && flex4 > flexbaseline*/){
+    }
+    if(flex1 > (flexbaseline + 50) && flex2 > (flexbaseline + 50) && flex3 > (flexbaseline + 50) && flex4 < (flexbaseline + 50)){
       sawToothTripleNote(0,1,2);
-    }/*
-    if(flex1 < flexbaseline && flex2 < flexbaseline && flex3 > flexbaseline && flex4 < flexbaseline){
+    }
+    if(flex1 > (flexbaseline + 50) && flex2 > (flexbaseline + 50) && flex3 < (flexbaseline + 50) && flex4 > (flexbaseline + 50)){
       sawToothTripleNote(0,1,3);
     }
-    if(flex1 < flexbaseline && flex2 > flexbaseline && flex3 < flexbaseline && flex4 < flexbaseline){
+    if(flex1 > (flexbaseline + 50) && flex2 < (flexbaseline + 50) && flex3 > (flexbaseline + 50) && flex4 > (flexbaseline + 50)){
       sawToothTripleNote(0,2,3);
     }
-    if(flex1 > flexbaseline && flex2 < flexbaseline && flex3 < flexbaseline && flex4 < flexbaseline){
+    if(flex1 > (flexbaseline + 50) && flex2 > (flexbaseline + 50) && flex3 > (flexbaseline + 50) && flex4 < (flexbaseline + 50)){
       sawToothTripleNote(0,1,2);
     }
-    if(flex1 < flexbaseline && flex2 < flexbaseline && flex3 < flexbaseline && flex4 < flexbaseline){
+    if(flex1 > (flexbaseline + 50) && flex2 > (flexbaseline + 50) && flex3 > (flexbaseline + 50) && flex4 > (flexbaseline + 50)){
       sawToothQuadNote(0,1,2,3);
-    }*/
+    }
   flex1 = 0;
   flex2 = 0;
   flex3 = 0;
-  //flex4 = 0;
+  flex4 = 0;
 
   for(int i = 0; i < 5; i++){
     flex1 += analogRead(0);
     flex2 += analogRead(1);
     flex3 += analogRead(2);
-    //flex4 += analogRead(3);
+    flex4 += analogRead(3);
   }
   flex1 = flex1/5;
   flex2 = flex2/5;
   flex3 = flex3/5;
-  //flex4 = flex4/5;
+  flex4 = flex4/5;
   }
-
-  /*if((millis()/50) > x){
-    if(y%2 == 0){
-      a = a/2;
-    }else{
-      a = a*2;
-    }
-    y = y + 1;
-    x = x + 1;
-  }*/
 }
 
 void sawToothNote(int note){
@@ -186,86 +176,12 @@ void sawToothTripleNote(int note1, int note2, int note3){
       sawToothNote((2*note3)/3);
       return;
 }
-/*
 void sawToothQuadNote(int note1, int note2, int note3, int note4){
-      digitalWrite(9, HIGH);
-      delayMicroseconds(notes[note1]);
-      digitalWrite(9, LOW);
-      delayMicroseconds(6*notes[note1]);
-      digitalWrite(9, HIGH);
-      delayMicroseconds(2*notes[note1]);
-      digitalWrite(9, LOW);
-      delayMicroseconds(3*notes[note1]);
-      digitalWrite(9, HIGH);
-      delayMicroseconds(4*notes[note1]);
-      digitalWrite(9, LOW);
-      delayMicroseconds(3*notes[note1]);
-      digitalWrite(9, HIGH);
-      delayMicroseconds(2*notes[note1]);
-      digitalWrite(9, LOW);
-      delayMicroseconds(6*notes[note1]);
-      digitalWrite(9, HIGH);
-      delayMicroseconds(notes[note1]);
-      digitalWrite(9, LOW);
-      digitalWrite(9, HIGH);
-      delayMicroseconds(notes[note2]);
-      digitalWrite(9, LOW);
-      delayMicroseconds(6*notes[note2]);
-      digitalWrite(9, HIGH);
-      delayMicroseconds(2*notes[note2]);
-      digitalWrite(9, LOW);
-      delayMicroseconds(3*notes[note2]);
-      digitalWrite(9, HIGH);
-      delayMicroseconds(4*notes[note2]);
-      digitalWrite(9, LOW);
-      delayMicroseconds(3*notes[note2]);
-      digitalWrite(9, HIGH);
-      delayMicroseconds(2*notes[note2]);
-      digitalWrite(9, LOW);
-      delayMicroseconds(6*notes[note2]);
-      digitalWrite(9, HIGH);
-      delayMicroseconds(notes[note2]);
-      digitalWrite(9, LOW);
-      digitalWrite(9, HIGH);
-      delayMicroseconds(notes[note3]);
-      digitalWrite(9, LOW);
-      delayMicroseconds(6*notes[note3]);
-      digitalWrite(9, HIGH);
-      delayMicroseconds(2*notes[note3]);
-      digitalWrite(9, LOW);
-      delayMicroseconds(3*notes[note3]);
-      digitalWrite(9, HIGH);
-      delayMicroseconds(4*notes[note3]);
-      digitalWrite(9, LOW);
-      delayMicroseconds(3*notes[note3]);
-      digitalWrite(9, HIGH);
-      delayMicroseconds(2*notes[note3]);
-      digitalWrite(9, LOW);
-      delayMicroseconds(6*notes[note3]);
-      digitalWrite(9, HIGH);
-      delayMicroseconds(notes[note3]);
-      digitalWrite(9, LOW);
-      digitalWrite(9, HIGH);
-      delayMicroseconds(notes[note4]);
-      digitalWrite(9, LOW);
-      delayMicroseconds(6*notes[note4]);
-      digitalWrite(9, HIGH);
-      delayMicroseconds(2*notes[note4]);
-      digitalWrite(9, LOW);
-      delayMicroseconds(3*notes[note4]);
-      digitalWrite(9, HIGH);
-      delayMicroseconds(4*notes[note4]);
-      digitalWrite(9, LOW);
-      delayMicroseconds(3*notes[note4]);
-      digitalWrite(9, HIGH);
-      delayMicroseconds(2*notes[note4]);
-      digitalWrite(9, LOW);
-      delayMicroseconds(6*notes[note4]);
-      digitalWrite(9, HIGH);
-      delayMicroseconds(notes[note4]);
-      digitalWrite(9, LOW);
-      delayMicroseconds(6*(notes[note1] + notes[note2] + notes[note3] + notes[note4]));
+      sawToothNote((note1)/2);
+      sawToothNote((note2)/2);
+      sawToothNote((note3)/2);
+      sawToothNote((note4)/2);
       return;
-}*/
+}
 
 
